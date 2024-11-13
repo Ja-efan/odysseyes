4fbf6a0a8b1e8eaa1cb43a9c4a1dcec04d22a88e
JAEHWAN <woghks1213@gmail.com>
2024-11-13 14:36:21 +0900

    Update: tools.py 함수 추가

--- recommend/func/tools.py
+++ recommend/func/tools.py
@@ -1,3 +1,4 @@
+import os 
 import json 
 
 def print_json(data:json):
@@ -8,3 +9,25 @@ def print_json(data:json):
     """
     pretty_json = json.dumps(data, indent=4, ensure_ascii=False)
     print(pretty_json)
+
+
+def find_target_directory(target_dir_name):
+    current_path = os.getcwd()
+    while True:
+        # 상위 디렉토리에서 대상 디렉토리 이름을 찾음
+        parent_path, current_dir = os.path.split(current_path)
+        
+        if current_dir == target_dir_name:
+            return current_path
+        
+        if parent_path == current_path:  # 최상위 디렉토리에 도달하면 종료
+            break
+        
+        # 상위 디렉토리로 이동
+        current_path = parent_path
+    
+    return None  # 대상 디렉토리를 찾지 못한 경우
+
+
+def get_project_root_path(proejct_directory_name: str='odysseyes'):
+    return  find_target_directory(target_dir_name=proejct_directory_name)
