9ed38baeb176652ae1f464424624cf0be8e74844
kwon <qja1998@naver.com>
2024-11-15 01:16:36 +0900

    add: csv for map plot

--- func/map_vis.py
+++ func/map_vis.py
@@ -20,21 +20,25 @@ theme="cartodbpositron"
 attr=""
 
 
-
 def bj_navi(gb):
     ## 목적지 분류 별 방문건수 시각화_공주시 데이터
     # 데이터 로드
-    
-    if gb:
-        df = pd.read_csv(f"./data/{gb}_bj_navi_전처리데이터.csv")
-        zoom_start = 12
-    else:
-        df = pd.read_csv(f"./data/bj_navi_전처리데이터.csv")
-        zoom_start = 7
+
+    df = pd.read_csv(f"./data/{gb}_bj_navi_전처리데이터.csv")
 
     df = df.groupby(
         ['대분류', '소분류', '목적지명','목적지읍면동명', '목적지X좌표', '목적지Y좌표'], as_index=False
     )['방문건수'].sum()
+
+    if gb == 'g':
+        region_name = "공주시"
+        zoom_start = 12
+    elif gb == 'b':
+        region_name = '부여군'
+        zoom_start = 12
+    else:
+        region_name = '전국'
+        zoom_start = 7
 
     # 지도 생성 (중심을 대략적인 평균 좌표로 설정)
     map_center = [df['목적지Y좌표'].mean(), df['목적지X좌표'].mean()]
@@ -43,7 +47,6 @@ def bj_navi(gb):
     # 붉은 계열 색상 그라데이션 설정 (방문건수에 따른 색상 설정)
     # Reds_09, YlOrRd_03, Set3_12, Spectral_03
     colormap = linear.YlOrRd_03.scale(df['방문건수'].min(), df['방문건수'].max())
-    # print(colormap)
 
     # 방문건수에 따른 오름차순 정렬 (작은 값이 먼저 그려지도록)
     df_sorted = df.sort_values(by='방문건수', ascending=True)
@@ -79,7 +82,15 @@ def bj_navi(gb):
     # 레이어 컨트롤 추가 (레이어 선택 가능)
     folium.LayerControl().add_to(m)
 
-    region_name = "공주시" if gb == 'g' else "부여군"
+    if gb == 'g':
+        region_name = "공주시"
+        zoom_start = 12
+    elif gb == 'b':
+        region_name = '부여군'
+        zoom_start = 12
+    else:
+        region_name = '전국'
+        zoom_start = 7
 
     return m, f"목적지 분류 별 방문건수 시각화_{region_name} 데이터"
 
@@ -87,16 +98,10 @@ def bj_navi(gb):
     # # 지도 저장
     # m.save('map_전처리_count.html')
 
-
 def togo_count(gb):
     ## 방문건수 시각화_공주시 데이터
     # 데이터 로드
-    if gb:
-        df = pd.read_csv(f"./data/{gb}_togo_count.csv")
-        zoom_start = 12
-    else:
-        df = pd.read_csv(f"./data/togo_count.csv")
-        zoom_start = 7
+    df = pd.read_csv(f"./data/{gb}_togo_count.csv")
 
     # 분위수 계산 (20분위수, 40분위수, 60분위수, 80분위수, 100분위수)
     quantiles = df['방문건수'].quantile([0.2, 0.4, 0.6, 0.8, 1.0])
@@ -118,6 +123,15 @@ def togo_count(gb):
 
     # 결과 출력
     df.head()
+    if gb == 'g':
+        region_name = "공주시"
+        zoom_start = 12
+    elif gb == 'b':
+        region_name = '부여군'
+        zoom_start = 12
+    else:
+        region_name = '전국'
+        zoom_start = 7
 
     # 지도 생성 (중심을 대략적인 평균 좌표로 설정)
     map_center = [df['목적지Y좌표'].mean(), df['목적지X좌표'].mean()]
@@ -161,7 +175,15 @@ def togo_count(gb):
     # 레이어 컨트롤 추가 (레이어 선택 가능)
     folium.LayerControl().add_to(m)
 
-    region_name = "공주시" if gb == 'g' else "부여군"
+    if gb == 'g':
+        region_name = "공주시"
+        zoom_start = 12
+    elif gb == 'b':
+        region_name = '부여군'
+        zoom_start = 12
+    else:
+        region_name = '전국'
+        zoom_start = 7
 
     return m, f"방문건수 시각화_{region_name} 데이터"
 
@@ -172,12 +194,7 @@ def togo_count(gb):
 def not_togo_count(gb):
     ## 방문건수 시각화_공주시데이터_외부유입 방문객
     # 데이터 로드
-    if gb:
-        df = pd.read_csv(f"./data/{gb}_not_togo_count.csv")
-        zoom_start = 12
-    else:
-        df = pd.read_csv(f"./data/not_togo_count.csv")
-        zoom_start = 7
+    df = pd.read_csv(f"./data/{gb}_not_togo_count.csv")
 
     # 분위수 계산 (20분위수, 40분위수, 60분위수, 80분위수, 100분위수)
     quantiles = df['방문건수'].quantile([0.2, 0.4, 0.6, 0.8, 1.0])
@@ -199,6 +216,15 @@ def not_togo_count(gb):
 
     # 결과 출력
     df.head()
+    if gb == 'g':
+        region_name = "공주시"
+        zoom_start = 12
+    elif gb == 'b':
+        region_name = '부여군'
+        zoom_start = 12
+    else:
+        region_name = '전국'
+        zoom_start = 7
 
     # 지도 생성 (중심을 대략적인 평균 좌표로 설정)
     map_center = [df['목적지Y좌표'].mean(), df['목적지X좌표'].mean()]
@@ -242,7 +268,15 @@ def not_togo_count(gb):
     # 레이어 컨트롤 추가 (레이어 선택 가능)
     folium.LayerControl().add_to(m)
 
-    region_name = "공주시" if gb == 'g' else "부여군"
+    if gb == 'g':
+        region_name = "공주시"
+        zoom_start = 12
+    elif gb == 'b':
+        region_name = '부여군'
+        zoom_start = 12
+    else:
+        region_name = '전국'
+        zoom_start = 7
 
     return m, f"방문건수 시각화_{region_name}데이터_외부유입 방문객"
 
@@ -255,13 +289,7 @@ def fest_togo_count(gb):
     # 데이터 로드
     ## 축제기간 여부 / 방문지 인기도
     # 데이터 로드
-
-    if gb:
-        df = pd.read_csv(f"./data/{gb}_fest_togo_count.csv")
-        zoom_start = 12
-    else:
-        df = pd.read_csv(f"./data/fest_togo_count.csv")
-        zoom_start = 7
+    df = pd.read_csv("./data/b_fest_togo_count.csv")
 
     # 분위수 계산 (20분위수, 40분위수, 60분위수, 80분위수, 100분위수)
     quantiles = df['방문건수'].quantile([0.5])
@@ -282,6 +310,15 @@ def fest_togo_count(gb):
 
     # festival_period를 문자열로 변환 (숫자일 경우 대비)
     df['festival_period'] = df['festival_period'].astype(str)
+    if gb == 'g':
+        region_name = "공주시"
+        zoom_start = 12
+    elif gb == 'b':
+        region_name = '부여군'
+        zoom_start = 12
+    else:
+        region_name = '전국'
+        zoom_start = 7
 
     # 지도 생성 (중심을 대략적인 평균 좌표로 설정)
     map_center = [df['목적지Y좌표'].mean(), df['목적지X좌표'].mean()]
@@ -292,8 +329,6 @@ def fest_togo_count(gb):
 
     # 방문건수에 따른 오름차순 정렬 (작은 값이 먼저 그려지도록)
     df_sorted = df.sort_values(by='방문건수', ascending=True)
-
-    # print(df_sorted)
 
     # 'festival_period'와 '방문건수구간'에 따른 마커 그룹 만들기
     marker_groups = {}
@@ -339,7 +374,15 @@ def fest_togo_count(gb):
     # 지도 저장
     m.save('map_b_fest_togo_count.html')
 
-    region_name = "공주시" if gb == 'g' else "부여군"
+    if gb == 'g':
+        region_name = "공주시"
+        zoom_start = 12
+    elif gb == 'b':
+        region_name = '부여군'
+        zoom_start = 12
+    else:
+        region_name = '전국'
+        zoom_start = 7
 
     return m, f"[{region_name} 축제기간 여부 / 방문지 인기도"
 
@@ -349,18 +392,22 @@ def fest_togo_count(gb):
 def fest_not_togo_count(gb):
     ## 공주 외부유입 데이터_축제기간 내외, 목적지 분류 별
     # 데이터 로드
-    if gb:
-        df = pd.read_csv(f"./data/{gb}_fest_not_togo_count.csv")
-        zoom_start = 12
-    else:
-        df = pd.read_csv(f"./data/fest_not_togo_merged.csv")
-        zoom_start = 7
+    df = pd.read_csv(f"./data/{gb}_fest_not_togo_count.csv")
 
     # '목적지명', 'festival_period' 등을 기준으로 방문건수를 그룹화
     df = df.groupby(
         ['목적지명', '목적지X좌표', '목적지Y좌표', '목적지시군구명', '목적지읍면동명', '대분류', '중분류', '소분류', 'festival_period'], 
         as_index=False
     )['방문건수'].sum()
+    if gb == 'g':
+        region_name = "공주시"
+        zoom_start = 12
+    elif gb == 'b':
+        region_name = '부여군'
+        zoom_start = 12
+    else:
+        region_name = '전국'
+        zoom_start = 7
 
     # 지도 생성 (중심을 대략적인 평균 좌표로 설정)
     map_center = [df['목적지Y좌표'].mean(), df['목적지X좌표'].mean()]
@@ -410,7 +457,15 @@ def fest_not_togo_count(gb):
     # 레이어 컨트롤 추가 (레이어 선택 가능)
     folium.LayerControl().add_to(m)
 
-    region_name = "공주시" if gb == 'g' else "부여군"
+    if gb == 'g':
+        region_name = "공주시"
+        zoom_start = 12
+    elif gb == 'b':
+        region_name = '부여군'
+        zoom_start = 12
+    else:
+        region_name = '전국'
+        zoom_start = 7
 
     return m, f"{region_name} 외부유입 데이터_축제기간 내외, 목적지 분류 별"
 
@@ -419,39 +474,38 @@ def fest_not_togo_count(gb):
 
 
 
-# def fest_visit_count():
-#     # 'festival_period'의 값에 따른 방문건수 합계 구하기
-#     # 축제 전/중/후 값을 각각 따로 계산
-#     df = pd.read_csv(f"./data/{gb}_fest_visit_count.csv")
+def fest_visit_count(gb):
+    # 'festival_period'의 값에 따른 방문건수 합계 구하기
+    # 축제 전/중/후 값을 각각 따로 계산
+    df = pd.read_csv(f"./data/{gb}_fest_visit_count.csv")
 
-#     # festival_period 값을 모두 '전체기간'으로 변경
+    # festival_period 값을 모두 '전체기간'으로 변경
 
 
-#     # festival_period를 기준으로 데이터를 합치고, 방문건수는 합계, 나머지는 첫 번째 값으로 처리
-#     df_aggregated = df.groupby(
-#         ['목적지명_통합', '목적지X좌표', '목적지Y좌표'], as_index=False
-#     ).agg({
-#         '방문건수': 'sum',  # 방문건수는 합계로 처리
-#         '표시용행정구역': 'first',  # 나머지는 첫 번째 값으로 처리
-#         'festival_period': 'first',  # 모든 값이 '전체기간'으로 설정됨
-#         'is_weekend': 'first',
-#         '요일': 'first',
-#     })
+    # festival_period를 기준으로 데이터를 합치고, 방문건수는 합계, 나머지는 첫 번째 값으로 처리
+    df_aggregated = df.groupby(
+        ['목적지명_통합', '목적지X좌표', '목적지Y좌표'], as_index=False
+    ).agg({
+        '방문건수': 'sum',  # 방문건수는 합계로 처리
+        '표시용행정구역': 'first',  # 나머지는 첫 번째 값으로 처리
+        'festival_period': 'first',  # 모든 값이 '전체기간'으로 설정됨
+        'is_weekend': 'first',
+        '요일': 'first',
+    })
 
-#     df_aggregated['festival_period'] = '전체기간'
+    df_aggregated['festival_period'] = '전체기간'
 
-#     # 결과 확인
-#     print(df_aggregated.head())
-#     df_aggregated.to_csv('df_aggregated.csv', encoding='utf-8-sig', index=False)
+    # # 결과 확인
+    # df_aggregated.to_csv('df_aggregated.csv', encoding='utf-8-sig', index=False)
 
-#     # 열 순서를 맞추기 위해, df의 열 순서를 기준으로 df_aggregated 열 순서 맞춤
-#     df_aggregated = df_aggregated[df.columns]
+    # 열 순서를 맞추기 위해, df의 열 순서를 기준으로 df_aggregated 열 순서 맞춤
+    df_aggregated = df_aggregated[df.columns]
 
-#     # df와 df_aggregated를 행으로 결합 (즉, 데이터를 이어 붙임)
-#     df_combined = pd.concat([df, df_aggregated], ignore_index=True, axis=0)
+    # df와 df_aggregated를 행으로 결합 (즉, 데이터를 이어 붙임)
+    df = pd.concat([df, df_aggregated], ignore_index=True, axis=0)
 
-#     # 결과 확인
-#     print(df_combined.head())
+    # # 결과 확인
+    # print(df_combined.head())
 
 #     return m
 
@@ -459,18 +513,27 @@ def fest_not_togo_count(gb):
 #     # df_combined.to_csv('combined_output.csv', encoding='utf-8-sig', index=False)
 
 
-def combined_output(gb):
+# def combined_output(gb):
 
     ## 축제기간 여부에 따른 방문건수 시각화_공주시 데이터
     # 데이터 로드
-    df = pd.read_csv(f"./data/combined_output.csv")
+    # df = pd.read_csv(f"./data/{gb}_combined_output.csv")
 
     # festival_period가 숫자로 저장된 경우 문자열로 변환
     df['festival_period'] = df['festival_period'].astype(str)
+    if gb == 'g':
+        region_name = "공주시"
+        zoom_start = 12
+    elif gb == 'b':
+        region_name = '부여군'
+        zoom_start = 12
+    else:
+        region_name = '전국'
+        zoom_start = 7
 
     # 지도 생성 (중심을 대략적인 평균 좌표로 설정)
     map_center = [df['목적지Y좌표'].mean(), df['목적지X좌표'].mean()]
-    m = folium.Map(location=map_center, zoom_start=12, tiles=theme, attr=attr)
+    m = folium.Map(location=map_center, zoom_start=zoom_start, tiles=theme, attr=attr)
 
     # 붉은 계열 색상 그라데이션 설정 (방문건수에 따른 색상 설정)
     colormap = linear.YlOrRd_03.scale(df['방문건수'].min(), df['방문건수'].max())
@@ -483,7 +546,7 @@ def combined_output(gb):
     for group in df_sorted['festival_period'].unique():
         if group == '0': 
             aan = '축제기간 전'
-        elif group == '1':
+        elif group == '1': 
             aan = '축제기간 중'
         elif group == '2': 
             aan = '축제기간 후'
@@ -518,7 +581,17 @@ def combined_output(gb):
     # 레이어 컨트롤 추가 (레이어 선택 가능)
     folium.LayerControl().add_to(m)
 
-    return m, f"축제기간 여부에 따른 방문건수 시각화_데이터"
+    if gb == 'g':
+        region_name = "공주시"
+        zoom_start = 12
+    elif gb == 'b':
+        region_name = '부여군'
+        zoom_start = 12
+    else:
+        region_name = '전국'
+        zoom_start = 7
+
+    return m, f"축제기간 여부에 따른 방문건수 시각화_{region_name} 데이터"
 
     # # 지도 저장
     # m.save('map_fest_visit_count.html')
@@ -528,22 +601,23 @@ def wkd_visit_count(gb):
 
     ## 주말여부 / 축제기간여부 / 방문지인기도
     # 데이터 로드
-    if gb:
-        df = pd.read_csv(f"./data/{gb}_wkd_visit_count.csv")
-        zoom_start = 12
-    else:
-        df = pd.read_csv("./data/wkd_visit_count.csv")
-        zoom_start = 7
+    df = pd.read_csv(f"./data/{gb}_wkd_visit_count.csv")
 
     # 분위수 계산 (20분위수, 40분위수, 60분위수, 80분위수, 100분위수)
-    quantiles = df['방문건수'].quantile([0.5])
+    quantiles = df['방문건수'].quantile([0.2, 0.4, 0.6, 0.8, 1.0])
 
     # 각 행의 '방문건수'가 어느 분위수에 속하는지 확인하여 '방문건수구간' 컬럼에 저장
     def assign_visit_range(visit_count):
-        if visit_count <= quantiles[0.5]:
-            return "보통"
+        if visit_count <= quantiles[0.2]:
+            return 1
+        elif visit_count <= quantiles[0.4]:
+            return 2
+        elif visit_count <= quantiles[0.6]:
+            return 3
+        elif visit_count <= quantiles[0.8]:
+            return 4
         else:
-            return "인기"
+            return 5
 
     df['방문건수구간'] = df['방문건수'].apply(assign_visit_range)
 
@@ -552,6 +626,15 @@ def wkd_visit_count(gb):
 
     # festival_period를 문자열로 변환 (숫자일 경우 대비)
     df['festival_period'] = df['festival_period'].astype(str)
+    if gb == 'g':
+        region_name = "공주시"
+        zoom_start = 12
+    elif gb == 'b':
+        region_name = '부여군'
+        zoom_start = 12
+    else:
+        region_name = '전국'
+        zoom_start = 7
 
     # 지도 생성 (중심을 대략적인 평균 좌표로 설정)
     map_center = [df['목적지Y좌표'].mean(), df['목적지X좌표'].mean()]
@@ -572,11 +655,12 @@ def wkd_visit_count(gb):
             aaan = "주말"
         
         for period in df_sorted['festival_period'].unique():
-            if period == '1': 
-                aan = '축제기간'
-            else:
+            # 축제 기간에 따라 그룹명 설정
+            if period == 0:  # 이 부분을 '0'에서 숫자 0으로 변경
                 aan = '축제기간 외'
-            
+            else: 
+                aan = '축제기간 내'
+                    
             for group in df_sorted['방문건수구간'].unique():
                 # 마커 그룹의 이름을 주말 여부, 축제 기간, 방문건수구간을 조합하여 생성
                 show_group = (aaan == '주말') and (aan == '축제기간') and (group == '인기')  # 기본적으로 인기 장소만 표시
@@ -609,7 +693,15 @@ def wkd_visit_count(gb):
     # 레이어 컨트롤 추가 (레이어 선택 가능)
     folium.LayerControl().add_to(m)
 
-    region_name = "공주시" if gb == 'g' else "부여군"
+    if gb == 'g':
+        region_name = "공주시"
+        zoom_start = 12
+    elif gb == 'b':
+        region_name = '부여군'
+        zoom_start = 12
+    else:
+        region_name = '전국'
+        zoom_start = 7
 
     return m, f"{region_name} 주말여부 / 축제기간여부 / 방문지인기도"
 
@@ -617,10 +709,9 @@ def wkd_visit_count(gb):
     # m.save('map_wkd_visit_count.html')
 
 
-def region_part():
-
+def nationwide_plot():
     # 데이터 로드
-    df = pd.read_csv("./data/맵표시용_좌표파일.csv")
+    df = pd.read_csv("./data/tt_맵표시용_좌표파일.csv")
 
     # 지도 생성 (중심을 대략적인 평균 좌표로 설정)
     map_center = [df['목적지Y좌표'].mean(), df['목적지X좌표'].mean()]
@@ -636,7 +727,7 @@ def region_part():
         # 해당 시도의 방문건수 최소-최대값 계산 후 컬러맵 생성
         min_count = df_sorted[df_sorted['시도'] == group]['방문건수'].min()
         max_count = df_sorted[df_sorted['시도'] == group]['방문건수'].max()
-        colormaps[group] = linear.Reds_09.scale(min_count, max_count)
+        colormaps[group] = linear.YlOrRd_03.scale(min_count, max_count)
         
         # 충청남도 그룹만 기본 표시 (다른 그룹은 비활성화)
         show_group = True if '충청남도' == group else False
@@ -669,6 +760,6 @@ def region_part():
     folium.LayerControl().add_to(m)
 
     # # 지도 저장
-    # m.save('map_지역별_count.html')
+    # m.save('map_tt_그룹별_지역별_count.html')
 
-    return m, f"map_지역별_count"
+    return m, "map_tt_그룹별_지역별_count"
